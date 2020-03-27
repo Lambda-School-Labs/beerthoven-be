a734e9d9cc55e5966972517bf10c66c60d0528f1
eralpkor <eralp.ny@gmail.com>
2020-03-27 17:11:46 -0400

    :fire:

--- apollo/src/resolvers/Mutation.js
+++ apollo/src/resolvers/Mutation.js
@@ -6,8 +6,13 @@
  * @returns { import('../generated/prisma-client').UserPromise }
  */
 const createUser = (_, args, context) => {
-  console.log("createUser.args: %j", args)
-  // const userTaken = 
+  console.log("createUser.args: %j", args);
+  console.log('This is context: ', context);
+
+  // const userTaken = users.some(user => user.username === args.data.username);
+
+  // if (userTaken) throw new Error('Username is taken...') 
+
   const user = context.prisma.createUser(args.data);  
 
   return user;
