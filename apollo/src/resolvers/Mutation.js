f1d85f8a129b5030bcdf68666ceba4fa8d106ce4
eralpkor <eralp.ny@gmail.com>
2020-04-06 18:35:26 -0400

    Co-authored-by: Wesley Moody <wgm3190@gmail.com>

--- apollo/src/resolvers/Mutation.js
+++ apollo/src/resolvers/Mutation.js
@@ -8,22 +8,7 @@ const { prisma } = require("../generated/prisma-client")
  * @param {{ prisma: import('../generated/prisma-client').Prisma }} context
  * @returns { import('../generated/prisma-client').UserPromise }
  */
-// const createUser = (_, args, context) => {
-//   // console.log("createUser.args: %j", args);
-//   // console.log('This is context: ', context);
-//   // fs.appendFileSync('hello.txt', "\n you're awesome \n")
-//   // const userTaken = context.prisma.users.some(user => user.username === args.data.username);
 
-//   // if (userTaken) throw new Error('Username is taken...') 
-
-//   const user = context.prisma.createUser(args.data);  
-
-//   return user;
-// };
-
-// module.exports = {
-//   createUser,
-// };
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
     if (!args.data.username) throw new Error('User name required!');
@@ -65,10 +50,50 @@ const Mutation = {
     return prisma.createPerson(args.data, info)
   },
 
+  async deletePerson(parent, args, { prisma }, info) {
+    const findPerson = await prisma.$exists.person({ id: args.where.id });
+
+    if (!findPerson) throw new Error('Person not found...');
+
+    return prisma.deletePerson({ id: args.where.id });
+  },
+
+  async updatePerson(parent, args, { prisma }, info) {
+    if (!args.where.id) throw new Error('ID is required!');
+
+    const findPerson = await prisma.$exists.person({ id: args.where.id });
+
+    if (!findPerson) throw new Error('Person not found...');
+
+    return prisma.updatePerson({
+      where: {
+        id: args.where.id
+      },
+      data: args.data
+    })
+  },
+
   async createEvent(parent, args, { prisma }, info) {
     if (!args.data.event_name) throw new Error('Event name required!');
 
-    return prisma.createEvent(args.data, info)
+    return prisma.createEvent(args.data, info);
+  },
+
+  async updateEvent(parent, args, { prisma }, info) {
+    console.log(args.where.event_id)
+    if (!args.where.event_id) throw new Error('Enter event ID...');
+
+    const findEvent = await prisma.$exists.event({ event_id: args.where.event_id })
+
+    if (!findEvent) throw new Error('Event not found');
+
+    return prisma.updateEvent({
+      where: {
+        event_id: args.where.event_id
+      },
+      data: args.data
+    })
+
   },
 
   async createVendor(parent, args, { prisma }, info) {
@@ -77,9 +102,72 @@ const Mutation = {
     if (emailTaken) throw new Error('Email taken');
 
     return prisma.createVendor(args.data, info);
-  }
+  },
+  
+  async updateVendor(parent, args, { prisma }, info) {
+    if (!args.where.id) throw new Error('ID required...');
+
+    const findVendor = await prisma.$exists.vendor({ id: args.where.id });
+
+    if (!findVendor) throw new Error('Vendor not found...');
+
+    return prisma.updateVendor({
+      where: {
+        id: args.where.id
+      },
+      data: args.data
+    });
+  },
+
+  async deleteVendor(parent, args, { prisma }, info) {
+    const findVendor = await prisma.$exists.vendor({ id: args.where.id });
+
+    if (!findVendor) throw new Error('Vendor with ID not found...');
+
+    return prisma.deleteVendor({ id: args.where.id })
+  },
+
+  async createTalent(parent, args, { prisma }, info) {
+    if (!args.data.talent_name || !args.data.performance_type || !args.data.address) throw new Error('Required fields name, type, address!');
+
+    // const usernameTaken = await prisma.$exists.talent({email: args.data.email});
+
+    // if (usernameTaken) throw new Error('Email taken');
+    return prisma.createTalent(args.data);
+  },
+
+  async updateTalent(parent, args, { prisma }, info) {
+    if (!args.where.id) throw new Error('Talent ID is required!');
+
+    const findTalent = await prisma.$exists.talent({ id: args.where.id });
+
+    if (!findTalent) throw new Error('Talent not found...');
+
+    return prisma.updateTalent({
+      where: {
+        id: args.where.id
+      },
+      data: args.data
+    })
+  },
+
+  async deleteTalent(parent, args, { prisma }, info) {
+    const findTalent = await prisma.$exists.talent({ id: args.where.id });
+
+    if (!findTalent) throw new Error('Talent with ID not found...');
+
+    return prisma.deleteTalent({ id: args.where.id })
+  },
+
+
+
+
+
+
+
+
+
+
 }
-
-
 
 module.exports = Mutation;
--- apollo/src/resolvers/User.js
+++ apollo/src/resolvers/User.js
@@ -1,4 +1,21 @@
+const { prisma } = require("../generated/prisma-client")
 
-const User = {}
+const User = {
+  const user = async (parent, _, context) => {
+  console.log("User.person.parent: %j", parent)
+ 
+  const person = await context.prisma.user({
+    id: parent.id
+  }).person();
+
+  // console.log("User.person: %j", person)
+  console.log("Is this users ", user)
+
+logger.log('Is this working')
+
+
+  return user;
+
+}
 
 module.exports = User;
