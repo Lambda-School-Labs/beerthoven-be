820396103572350f38bb904c7003acbfa31aeb4f
eralpkor <eralp.ny@gmail.com>
2020-04-07 15:37:21 -0400

    Fixed deploy issue with datamodel

--- apollo/src/resolvers/Query.js
+++ apollo/src/resolvers/Query.js
@@ -5,35 +5,12 @@
  * @param {{ prisma: import('../generated/prisma-client').Prisma }} context
  * @returns { Promise }
  */
-// const users = async (_, args, context) => {
-//   console.log("Query.users.args: %j", args)
-
-//   const users = await context.prisma.users(null, '{id username}');
-
-//   console.log("Query.user: %j", users)
-  
-//   return [];
-// };
-
 /**
  * @param {{ where: import('../generated/prisma-client').UserWhereInput }} args
  * @param {{ prisma: import('../generated/prisma-client').Prisma }} context
  * @returns { Promise }
  */
-// const users = async (_, args, context) => {
-//   console.log("Query.user.args: %j", args)
 
-//   const user = await context.prisma.users(args);
-
-//   console.log("Query.user: %j", user)
-  
-//   return user;
-// };
-
-// module.exports = {
-//   //user,
-//   users
-// };
 
 const Query = {
   async users(parent, args, { prisma }, info) {
