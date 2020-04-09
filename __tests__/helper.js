ee37cf13e125fab097fda404bd18a3b1bc12c2ae
wesley.moody <wgm3190@gmail.com>
2020-04-09 16:56:53 -0500

    update datamodel

---
+++ __tests__/helper.js
@@ -0,0 +1,34 @@
+const { ApolloServer } = require('apollo-server');
+const { createTestClient } = require('apollo-server-testing');
+const typeDefs = require('../prisma/')
+import resolvers from( '../apollo/src/resolvers/Mutation')
+
+const { query, mutate } = createTestClient(server);
+
+//  mocks: true  `ignores resolvers and mocks them out with random data`
+
+const createTestServer = ctx => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    mockEntireSchema: false,
+    mocks: false,
+    context: () => ctx
+  })
+
+  return createTestClient(server)
+}
+module.exports =  createTestServer
+
+
+
+
+// query({
+//   query: GET_EVENT,
+//   variables: { id: "ck8qd0ecb000v07523uvv7sah" }
+// });
+
+// mutate({
+//   mutation: UPDATE_EVENT,
+//   variables: { id: "ck8qd0ecb000v07523uvv7sah", event_name  }
+// })
