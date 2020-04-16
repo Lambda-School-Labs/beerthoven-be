1a1a5986a1d87bb9af45062a5a122469a20b37e5
Bernie Durfee <berniedurfee@users.noreply.github.com>
2020-04-16 10:24:05 -0400

    Don't need Prisma

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@
 const {defaults} = require('jest-config');
 module.exports = {
   // This is a monorepo, so we need to tell Jest where the actual projects are
-  projects: ["apollo", "prisma"],
+  projects: ["apollo"],
 };
